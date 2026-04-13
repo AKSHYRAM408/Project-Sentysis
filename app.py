4ee7598d994df599b370b7345a4bab4781b2784e
Akshyram Mahendiran <137417218+AKSHYRAM408@users.noreply.github.com>
2026-04-13 20:37:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import re
 import os
 import requests
 import streamlit as st
-from dotenv import load_dotenv
 from selenium import webdriver
+from dotenv import load_dotenv
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
